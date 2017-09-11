316719fc98dcf8a54237d5fef13cf5786a06be17
Vikrant Varma <vikrant.varma94@gmail.com>
2017-09-11 14:38:02 +0100

    Add Maps extension
    Use Google Maps as the backend. We could use something libre, but
    realistically gmaps is much better, and also free of charge.

--- mediawiki/LocalSettings.php
+++ mediawiki/LocalSettings.php
@@ -448,3 +448,7 @@ $wgGroupPermissions['*']['abusefilter-log'] = true;
 $wgGroupPermissions['sysop']['abusefilter-private'] = true;
 $wgGroupPermissions['sysop']['abusefilter-modify-restricted'] = true;
 $wgGroupPermissions['sysop']['abusefilter-revert'] = true;
+
+# Maps extension; installed through composer
+$GLOBALS['egMapsGMaps3ApiKey'] = getenv('GOOGLE_MAPS_API_KEY', true);
+$GLOBALS['egMapsDefaultService'] = 'googlemaps3';
