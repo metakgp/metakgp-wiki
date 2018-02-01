f87f56ce58a2ba7843eb67f542f37b662a3026fc
Vikrant Varma <vikrant.varma94@gmail.com>
2018-02-01 02:10:30 +0000

    Configure MinervaNeue as mobile theme
    Previously this was bundled with MobileExtension, but now we need to
    download and configure it manually.

--- mediawiki/install_extensions.sh
+++ mediawiki/install_extensions.sh
@@ -16,11 +16,21 @@ declare -a extension_names=( \
     googleAnalytics \
 )
 
+declare -a skin_names=( \
+    MinervaNeue \
+)
+
 MEDIAWIKI_RELEASE=REL1_30
 
 function fetch_extension_url() {
     curl -s "https://www.mediawiki.org/wiki/Special:ExtensionDistributor?extdistname=$1&extdistversion=$MEDIAWIKI_RELEASE" \
         | grep -oP 'https://extdist.wmflabs.org/dist/extensions/.*?.tar.gz' \
+        | head -1
+}
+
+function fetch_skin_url() {
+    curl -s "https://www.mediawiki.org/wiki/Special:SkinDistributor?extdistname=$1&extdistversion=$MEDIAWIKI_RELEASE" \
+        | grep -oP 'https://extdist.wmflabs.org/dist/skins/.*?.tar.gz' \
         | head -1
 }
 
@@ -31,6 +41,14 @@ for extension_name in "${extension_names[@]}"; do
     wget -q $versioned_extension_url
     tar -xzf "$versioned_extension_name"
     mv $extension_name /srv/mediawiki/extensions/
+done
+
+for skin_name in "${skin_names[@]}"; do
+    versioned_skin_url=$(fetch_skin_url $skin_name)
+    versioned_skin_name=$(echo $versioned_skin_url | awk -F"/" '{print $(NF)}')
+    wget -q $versioned_skin_url
+    tar -xzf "$versioned_skin_name"
+    mv $skin_name /srv/mediawiki/skins/
 done
 
 # Get RecentPages from Github
