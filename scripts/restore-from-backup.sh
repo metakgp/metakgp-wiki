26c4c49c41ee53dd85f2f0846b9a2796c48489ed
Siddharth Kannan <kannan.siddharth12@gmail.com>
2018-03-14 19:54:33 +0530

    use the MYSQL_CONTAINER variable instead of docker-compose

--- scripts/restore-from-backup.sh
+++ scripts/restore-from-backup.sh
@@ -5,4 +5,4 @@ set -xe
 source .env
 MYSQL_CONTAINER=$(docker ps --format '{{ .Names }}' | grep mysql)
 docker cp $1 $MYSQL_CONTAINER:metakgp_wiki_db.sql
-docker-compose exec mysql sh -c 'mysql -u metakgp_user -p'$MYSQL_PASSWORD' metakgp_wiki_db < metakgp_wiki_db.sql'
+docker exec $MYSQL_CONTAINER sh -c 'mysql -u metakgp_user -p'$MYSQL_PASSWORD' metakgp_wiki_db < metakgp_wiki_db.sql'
