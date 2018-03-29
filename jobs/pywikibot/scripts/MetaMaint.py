ff9680b4aedd3cd148911393df6b162ef7de1ee0
Navaneeth Suresh <33940798+TheMousePotato@users.noreply.github.com>
2018-03-29 12:58:45 +0530

    Create MetaMaint.py
    TODO:
    * Add Random_package template to sidebar of homepage(which I don't have access)
    * Deploy and make it run once in a day.

---
+++ jobs/pywikibot/scripts/MetaMaint.py
@@ -0,0 +1,24 @@
+# MetaMaint - script to add a random package to MetaKGP homepage.
+
+import pywikibot
+from random import randint
+from datetime import date
+
+def main():
+    site = pywikibot.Site()
+    
+    # Read package blurbs
+    rpage = pywikibot.Page(site, 'Package_blurbs')
+    data = rpage.text
+    packages = data.split('<hr>')[1:]
+    
+    # Generate a random package blurb
+
+    header = "<noinclude>This page is automatically generated. Changes will be overwritten, so '''do not modify'''.</noinclude>\n"
+
+    wpage = pywikibot.Page(site, 'Template:Random_package')
+    wpage.text = header+packages[randint(1, len(packages))]
+    wpage.save(('Updated on ' + date.today().strftime('%B %d, %Y')))
+
+if __name__ == '__main__':
+    main()
