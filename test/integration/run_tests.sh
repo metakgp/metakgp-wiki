a20f754125d9bbbf0e54aa1f070ab85656644fe2
Vikrant Varma <vikrant.varma94@gmail.com>
2019-07-17 00:15:20 +0100

    Update comment

--- test/integration/run_tests.sh
+++ test/integration/run_tests.sh
@@ -67,7 +67,7 @@ for i in {1..24}; do
     fi
 done
 
-# Find the random port that nginx is mapped to
+# Find the port that nginx is mapped to (needs to be 8080 as hard-coded in `.env`)
 NGINX_ADDR=$(docker-compose port nginx 80)
 
 info "Initializing database"
