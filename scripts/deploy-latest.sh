6ad60c8bdcafbcc4e234bce1b69ed6ec2d3057a5
Siddharth Kannan <mail@siddharthkannan.in>
2020-06-24 22:20:52 +0900

    Move to a function; check for executable existence

--- scripts/deploy-latest.sh
+++ scripts/deploy-latest.sh
@@ -12,13 +12,29 @@ set -xe
 deploy_message () {
 	local action=$1
 	case $action in
-		"start")
+		"deploy_start")
 			echo "Deploy begin: User $(whoami) is deploying to metakgp-wiki"
 			;;
-		"end")
-			echo "Deploy end: Completed deployment"
+		"downtime_start")
+			echo "Downtime begin"
+			;;
+		"downtime_end")
+			echo "Downtime end"
+			;;
+		"deploy_end")
+			echo "Deploy end"
 			;;
 	esac
+}
+
+notify_slack () {
+	message="${1}"
+	if [[ -n "$SLACK_NOTIFICATIONS_URL" ]];
+	then
+		curl -s -H 'content-type: application/json' \
+			 -d "{ \"text\": \"${message}\" }" \
+			 "$SLACK_NOTIFICATIONS_URL"
+	fi
 }
 
 # TODO: Better as parameters to the deploy function?
@@ -27,6 +43,8 @@ deploy_branch="origin/master"
 
 deploy () {
 	local go="${1}"
+
+	notify_slack "$(deploy_message deploy_start)"
 
 	local source_dir=$(pwd)
 	echo "START: metakgp-wiki deploy"
@@ -48,14 +66,15 @@ deploy () {
 	local docker_compose="docker-compose"
 	local docker="docker"
 
-	# TODO: Fail if docker or docker-compose not found
-	${docker} version > /dev/null
-	${docker_compose} version > /dev/null
+	[[ -x "$(which ${docker})" ]]
+	[[ -x "$(which ${docker_compose})" ]]
 
 	echo "STEP: Running backup job"
-	local backup_container_exec="${docker_compose} -f docker-compose.yml \
+
+	local docker_compose_override="${docker_compose} -f docker-compose.yml \
 					  -f docker-compose.override.yml \
-					  -f docker-compose.prod.yml exec backup"
+					  -f docker-compose.prod.yml"
+	local backup_container_exec="${docker_compose_override} exec backup"
 
 	${backup_container_exec} ./run_backup.sh 2>/dev/null
 
@@ -87,13 +106,7 @@ deploy () {
 	echo "STEP: Build docker images for the new configuration"
 	${docker_compose_override} build
 
-	# TODO: Move to a function
-	if [[ -n "$SLACK_NOTIFICATIONS_URL" ]];
-	then
-		curl -s -H 'content-type: application/json' \
-			 -d "{ \"text\": \"$(deploy_message start)\" }" \
-			 "$SLACK_NOTIFICATIONS_URL"
-	fi
+	notify_slack "$(deploy_message downtime_start)"
 
 	echo "STEP: Bring all containers down"
 	${docker_compose_override} down
@@ -108,14 +121,13 @@ deploy () {
 
 	echo "STEP: All containers are up (Downtime end) $(date +%s)"
 
-	# TODO: Move to a function
-	if [[ -n "$SLACK_NOTIFICATIONS_URL" ]];
-	then
-		curl -s -H 'content-type: application/json' \
-			 -d "{ \"text\": \"$(deploy_message end)\" }" \
-			 "$SLACK_NOTIFICATIONS_URL"
-	fi
+	notify_slack "$(deploy_message downtime_end)"
 
+	echo "STEP: Run maintenance/update.php to update DB schema, if required"
+	local php_container_exec="${docker_compose_override} exec php"
+	${php_container_exec} /srv/mediawiki/maintenance/update.php --quick
+
+	notify_slack "$(deploy_message deploy_end)"
 	echo "END: deploying metakgp-wiki"
 }
 
