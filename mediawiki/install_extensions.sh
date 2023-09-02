2ca5aa58250349d8b91e708976c67369bca7742f
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2023-09-03 01:11:22 +0530

    feat: simplified install_extensions.sh

--- mediawiki/install_extensions.sh
+++ mediawiki/install_extensions.sh
@@ -14,11 +14,6 @@ declare -a extension_names=( \
     SimpleChanges \
 )
 
-declare -A extension_version_overrides=( \
-)
-
-declare -a skin_names=()
-
 MEDIAWIKI_RELEASE=REL1_40
 
 function fetch_extension_url() {
@@ -36,20 +31,9 @@ function fetch_skin_url() {
 cd /tmp
 for extension_name in "${extension_names[@]}"; do
     version=$MEDIAWIKI_RELEASE
-    if [[ -v "extension_version_overrides[$extension_name]" ]]; then
-	version=${extension_version_overrides[$extension_name]}
-    fi
     versioned_extension_url=$(fetch_extension_url $extension_name $version)
     versioned_extension_name=$(echo $versioned_extension_url | awk -F"/" '{print $(NF)}')
     wget -q $versioned_extension_url
     tar -xzf "$versioned_extension_name"
     mv $extension_name /srv/mediawiki/extensions/
-done
-
-for skin_name in "${skin_names[@]}"; do
-    versioned_skin_url=$(fetch_skin_url $skin_name $MEDIAWIKI_RELEASE)
-    versioned_skin_name=$(echo $versioned_skin_url | awk -F"/" '{print $(NF)}')
-    wget -q $versioned_skin_url
-    tar -xzf "$versioned_skin_name"
-    mv $skin_name /srv/mediawiki/skins/
 done
