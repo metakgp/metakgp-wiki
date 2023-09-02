c4693fe466622bff9812d25d1e4e593bc9c733f4
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2023-09-03 01:21:58 +0530

    refactor: moved composer extension installation into install_extensions.sh

--- mediawiki/install_extensions.sh
+++ mediawiki/install_extensions.sh
@@ -2,6 +2,9 @@
 
 set -xe
 
+MEDIAWIKI_RELEASE=REL1_40
+
+# Install extensions from extension distributor
 declare -a extension_names=( \
     ArticleFeedbackv5 \
     CheckUser \
@@ -13,8 +16,6 @@ declare -a extension_names=( \
     WikimediaMessages \
     SimpleChanges \
 )
-
-MEDIAWIKI_RELEASE=REL1_40
 
 function fetch_extension_url() {
     curl -s "https://www.mediawiki.org/wiki/Special:ExtensionDistributor?extdistname=$1&extdistversion=$2" \
@@ -28,12 +29,25 @@ function fetch_skin_url() {
         | head -1
 }
 
-cd /tmp
+pushd /tmp
 for extension_name in "${extension_names[@]}"; do
     version=$MEDIAWIKI_RELEASE
     versioned_extension_url=$(fetch_extension_url $extension_name $version)
     versioned_extension_name=$(echo $versioned_extension_url | awk -F"/" '{print $(NF)}')
+
     wget -q $versioned_extension_url
     tar -xzf "$versioned_extension_name"
     mv $extension_name /srv/mediawiki/extensions/
 done
+popd
+
+# Install extensions from composer
+declare -a composer_extension_names=( \
+    mediawiki/maps \
+    mediawiki/simple-batch-upload \
+)
+
+for extension_name in "${composer_extension_names[@]}"; do
+    php composer.phar require "${extension_name}"
+done
+php composer.phar update
