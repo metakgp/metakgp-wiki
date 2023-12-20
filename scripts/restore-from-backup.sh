200dfccc3761ce36ab918ffd183522b3366c1cf9
Dhruv Patel <140754596+Dhruv-Pattel@users.noreply.github.com>
2023-12-20 22:44:05 +0530

    Update restore-from-backup.sh
    made the required changes

--- scripts/restore-from-backup.sh
+++ scripts/restore-from-backup.sh
@@ -7,11 +7,11 @@ MYSQL_CONTAINER=$(docker ps --format '{{ .Names }}' | grep mysql)
 docker cp $1 $MYSQL_CONTAINER:metakgp_wiki_db.sql
 docker exec $MYSQL_CONTAINER sh -c 'mysql -u metakgp_user -p'$MYSQL_PASSWORD' metakgp_wiki_db < metakgp_wiki_db.sql'
 
-result=$(find /backup -type f -name "images")
+images_exist=$(find ./backup -type f -name "images")
 MEDIAWIKI_CONTAINER=$(docker ps --format '{{ .Names }}' | grep mediawiki)
 
-if [ -n "$result" ]
+if [ -n "$images_exist" ]
 then
-    docker cp $result $MEDIAWIKI_CONTAINER:/srv/static/
+    docker cp $images_exist $MEDIAWIKI_CONTAINER:/srv/static/
     docker exec $MEDIAWIKI_CONTAINER chown -LR www-data:www-data /srv/static/images
 fi
