116dd35545aa2d498ea55cb55b46e61014dd6fd2
Shikhar Soni <139053348+shikharish@users.noreply.github.com>
2024-03-10 02:24:06 +0530

    [jobs/backup]Feat: delete Dropbox backups older than 30 days (#127)
    * feat: delete dropbox backups older than 30 days
    
    * make script executable
    
    Signed-off-by: Shikhar Soni <shikharish05@gmail.com>
    
    * feat: separate log files for backups and backup rotation
    
    Signed-off-by: Shikhar Soni <shikharish05@gmail.com>
    
    * more log rotation
    
    Signed-off-by: Shikhar Soni <shikharish05@gmail.com>
    
    * more robust backup rotate script
    
    Signed-off-by: Shikhar Soni <shikharish05@gmail.com>
    
    ---------
    
    Signed-off-by: Shikhar Soni <shikharish05@gmail.com>

---
+++ jobs/backup/rotate_backups.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import dropbox
+import os
+import datetime
+
+app_key = os.environ["DROPBOX_APP_KEY"]
+app_secret = os.environ["DROPBOX_APP_SECRET"]
+access_token = os.environ["DROPBOX_ACCESS_TOKEN"]
+refresh_token = os.environ["DROPBOX_REFRESH_TOKEN"]
+client = dropbox.Dropbox(
+    app_key=app_key,
+    app_secret=app_secret,
+    oauth2_access_token=access_token,
+    oauth2_refresh_token=refresh_token,
+)
+
+BACKUP_FOLDER_PATH = ""
+has_more_files = True
+cursor = None
+result = None
+files = []
+now = datetime.datetime.now()
+counter = 0
+
+print("Fetching files from Dropbox...")
+while has_more_files:
+    if cursor is None:
+        result = client.files_list_folder(BACKUP_FOLDER_PATH)
+    else:
+        result = client.files_list_folder_continue(cursor=cursor)
+    cursor = result.cursor
+    has_more_files = result.has_more
+    files.extend(result.entries)
+
+print("Starting rotation")
+number_of_files = len(files)
+for file in files:
+    if file.name.find("metakgp_wiki") == -1:
+        continue
+    file_timestamp = file.client_modified
+    days_old = (now - file_timestamp).days
+    if days_old > 30 and number_of_files > 30:
+        client.files_delete(file.path_display)
+        counter += 1
+
+print(counter + " backup file(s) successfully deleted.")
