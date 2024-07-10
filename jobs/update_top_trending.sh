a97bcbb59d0c03f3cf588b4e38a893e4442f7725
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2024-07-11 00:02:39 +0530

    fix: use the pypi pywikibot

--- jobs/update_top_trending.sh
+++ jobs/update_top_trending.sh
@@ -6,5 +6,5 @@ echo "Updating Top and Trending Pages"
 
 cd /root/pywikibot
 export METAKGP_BOT_NAME=batman
-timeout 10s python pwb.py login
-timeout 30s python pwb.py updatestatistics
+timeout 10s pwb login
+timeout 30s pwb updatestatistics
