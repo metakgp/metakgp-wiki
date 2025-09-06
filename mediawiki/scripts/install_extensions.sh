ee85870ec9e2976b65173ab0c4afe3c130a036b4
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2025-09-06 19:58:44 +0530

    fix: oopsie

--- mediawiki/scripts/install_extensions.sh
+++ mediawiki/scripts/install_extensions.sh
@@ -55,7 +55,7 @@ declare -a composer_extension_names=( \
     mediawiki/simple-batch-upload \
 )
 
-php composer.phar config --no-plugins allow-plugins.composer/installers
+php composer.phar config --no-plugins allow-plugins.composer/installers true
 
 for extension_name in "${composer_extension_names[@]}"; do
     php composer.phar require "${extension_name}"
